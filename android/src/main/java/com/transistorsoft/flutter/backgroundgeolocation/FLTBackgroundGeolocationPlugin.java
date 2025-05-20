8b302a54478aa1cdc826ed0d62db9bd55b64ab9b
ziyad-aljohani <135967503+ziyad-aljohani@users.noreply.github.com>
2025-05-20 16:08:31 +0300

    Update FLTBackgroundGeolocationPlugin.java

--- android/src/main/java/com/transistorsoft/flutter/backgroundgeolocation/FLTBackgroundGeolocationPlugin.java
+++ android/src/main/java/com/transistorsoft/flutter/backgroundgeolocation/FLTBackgroundGeolocationPlugin.java
@@ -14,17 +14,17 @@ public class FLTBackgroundGeolocationPlugin implements FlutterPlugin, ActivityAw
 
     @Override
     public void onAttachedToEngine(@NonNull FlutterPlugin.FlutterPluginBinding binding) {
-        BackgroundGeolocationModule.getInstance().onAttachedToEngine(binding.getApplicationContext(), binding.getBinaryMessenger());
+
     }
 
     @Override
     public void onDetachedFromEngine(@NonNull FlutterPlugin.FlutterPluginBinding binding) {
-        BackgroundGeolocationModule.getInstance().onDetachedFromEngine(binding.getBinaryMessenger());
+
     }
 
     @Override
     public void onAttachedToActivity(@NonNull ActivityPluginBinding activityPluginBinding) {
-        BackgroundGeolocationModule.getInstance().setActivity(activityPluginBinding.getActivity());
+
     }
 
     @Override
@@ -42,6 +42,6 @@ public class FLTBackgroundGeolocationPlugin implements FlutterPlugin, ActivityAw
 
     @Override
     public void onDetachedFromActivity() {
-        BackgroundGeolocationModule.getInstance().setActivity(null);
+
     }
 }
